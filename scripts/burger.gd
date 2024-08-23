d12d7c32c8597a3062a12e7d9f4fc7eb58ed2f4d
AstrlJelly <bdlawson115@gmail.com>
2024-08-23 17:42:15 -0400

    plugins, burger spritesheet
    switching to an ms paint/pixel art artstyle!! just gonna archive this as a fallback. hehehe

--- scripts/burger.gd
+++ scripts/burger.gd
@@ -1,17 +1,30 @@
 extends Node
 
+class_name Burger;
+
+static var rawBurgers : Array[Texture2D]
+static var cookedBurgers : Array[Texture2D]
+
 @export var sprite : Sprite2D
+@export var anim : AnimationPlayer
 @export var cookTime : float = 5
 var cookedPercentages : Vector2 = Vector2(0, 0)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	#sprite.material = mat.duplicate(true)
-	pass
+func ready() -> void:
+	# TODO: cache this stuff? not sure if there's a significant performance hit from accessing and setting stuff in init
+	var rawBurgers : Array[Texture2D] = sprite.material.get_shader_parameter("rawBurgers")
+	var cookedBurgers : Array[Texture2D] = sprite.material.get_shader_parameter("cookedBurgers")
+	var rng = RandomNumberGenerator.new()
+	#rng.randomize()
+	var rawRand : int = rng.randi_range(0, rawBurgers.size());
+	sprite.material.set_shader_parameter("whichRaw", rawRand)
+	#rng.randomize()
+	var cookedRand : int = rng.randi_range(0, rawBurgers.size());
+	sprite.material.set_shader_parameter("whichCooked", cookedRand)
+	print("rawRand == cookedRand : ", rawRand == cookedRand);
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	cookedPercentages.y += delta / cookTime;
-	cookedPercentages = cookedPercentages.clamp(Vector2.ZERO, Vector2.ONE);
-	print("sprite.Material cookedPercentages : ", sprite.material.get_shader_parameter("cookedPercentages"));
-	sprite.material.set_shader_parameter("cookedPercentages", cookedPercentages);
+# not called automatically, but should be called in the _process of the file inheriting from it.
+func process(delta: float) -> void:
+	cookedPercentages.y += delta / cookTime
+	cookedPercentages = cookedPercentages.clamp(Vector2.ZERO, Vector2.ONE)
+	sprite.material.set_shader_parameter("cookedPercentages", cookedPercentages)
---
+++ scripts/grill_burger.gd
@@ -0,0 +1,7 @@
+extends Burger
+
+func _ready() -> void:
+	ready();
+
+func _process(delta: float) -> void:
+	process(delta);
