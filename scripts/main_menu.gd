930cef454030397417e3adae7434aecc46f82bf4
AstrlJelly <bdlawson115@gmail.com>
2024-08-28 22:23:03 -0400

    ACTUALLY get usernames working
    i typed a variable wrong which caused literal hours of debugging :broken_heart: gonna commit, clean it up, then commit again

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -8,13 +8,14 @@ var peer = ENetMultiplayerPeer.new()
 
 @export var player_scene: PackedScene
 @export var lobby_player_scene: PackedScene
+@export var lobby_players_parent: Node
 
 @export var username_input: TextEdit
 @export var create_port_input: TextEdit
 @export var join_port_input: TextEdit
 
 func _ready() -> void:
-	#multiplayer.connected_to_server.connect(connected_to_server)
+	multiplayer.connected_to_server.connect(connected_to_server)
 	multiplayer.peer_connected.connect(peer_connected)
 
 func _on_create_game_pressed() -> void:
@@ -23,8 +24,13 @@ func _on_create_game_pressed() -> void:
 	if error:
 		printerr(error)
 		return
-	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
-	multiplayer.set_multiplayer_peer(peer)
+	setup_multiplayer()
+	#connected_to_server()
+	send_player_data(multiplayer.get_unique_id(), username_input.text)
+	
+	#var id = multiplayer.get_unique_id()
+	#send_player_data(id, username_input.text)
+	#add_player(id)
 	
 	#join_game()
 
@@ -33,34 +39,56 @@ func _on_join_game_pressed() -> void:
 	var address_port : PackedStringArray = join_ip_port.split(':')
 	if address_port.size() < 2 or !address_port[1].is_valid_int(): return
 	
-	peer.create_client(address_port[0], int(address_port[1]))
+	var error = peer.create_client(address_port[0], int(address_port[1]))
+	if error:
+		printerr(error)
+		return
+	setup_multiplayer()
+	#join_game()
+
+func setup_multiplayer():
+	join_create_buttons.hide()
 	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
 	multiplayer.set_multiplayer_peer(peer)
 	
-	#join_game()
+	#add_player(multiplayer.get_unique_id(), username_input.text)
 
+func connected_to_server():
+	var id : int = multiplayer.get_unique_id()
+	send_player_data.rpc(id, username_input.text)
+	#send_player_data.rpc_id(1, id, username_input.text)
+	#add_player.rpc_id(1, id)
 
 func peer_connected(id : int):
-	send_player_data.rpc(id, username_input.text)
+	#send_player_data.rpc(id, username_input.text)
+	#send_player_data.rpc(id, username_input.text)
+	pass
+
+@rpc("any_peer", "call_remote")
+func send_player_data(id : int, username : String):
+	if username.length() <= 0:
+		username = "Jimny_"+str(id)
+
+	if !PlayerData.Players.has(id):
+		var p = lobby_player_scene.instantiate()
+		p.name = str(id)
+		p.get_child(0).text = str(id)
+		p.get_child(1).text = username
+		lobby_players_parent.add_child(p)
+		PlayerData.Players[id] = username
+	
+	if multiplayer.is_server():
+		for pId in PlayerData.Players:
+			send_player_data.rpc(pId, PlayerData.Players[pId])
 
 @rpc("any_peer", "call_local")
-func send_player_data(id : int, username : String):
-	if !PlayerData.Players.has(id):
-		PlayerData.Players[id] = username
-
-#func _on_join_game_pressed() -> void:
-	#join_game()
-
-#func _add_player(id : int = 1):
-	#var name = username_input.text
-	#lobby.add_player(name if name.length() > 0 else "Jimny_"+str(id))
-
-#func join_game() -> void:
-	#join_create_buttons.hide()
-	#var username = username_input.text
-	#PlayerData.id = multiplayer.get_unique_id()
-	#PlayerData.username = username if username.length() > 0 else "Jimny_"+str(PlayerData.id)
-	#set_multiplayer_authority(PlayerData.id)
-	#var lobby_scene = load("res://assets/scenes/lobby.tscn").instantiate()
-	#get_tree().root.add_child(lobby_scene)
-	#self.hide()
+func add_player(id : int):
+	var username = PlayerData.Players[id]
+	var player = player_scene.instantiate()
+	player.name = str(id)
+	print(player.name)
+	player.player_name = username if username.length() > 0 else "Jimny_"+str(id)
+	var max_length : int = "WWWWWWWWWWWWWWWW".length()
+	player.name_label.text = "[center][font_size=100]" + (username if username.length() < max_length else username.substr(0, max_length))
+	#call_deferred("add_child", player)
+	add_child(player)
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,10 +4,14 @@ var player_name : String = ""
 @export var cursor : Node2D
 @export var name_label : RichTextLabel
 
+func _enter_tree() -> void:
+	set_multiplayer_authority(name.to_int())
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if is_multiplayer_authority():
-		cursor.position = get_global_mouse_position()
+	#if get_multiplayer_authority() == multiplayer.get_unique_id():
 		
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-			print(player_name);
+			cursor.position = get_global_mouse_position()
+			#print(player_name);
